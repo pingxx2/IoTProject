8de64a6b2ab01a1d665fd1b6495636e85fad4147
DESKTOP-P6ARO04\balsw <domi29@naver.com>
2022-12-20 11:55:07 +0900

    Add Tv for save channel values

--- app/src/main/java/com/example/iotproject/TabTvFragment.kt
+++ app/src/main/java/com/example/iotproject/TabTvFragment.kt
@@ -1,7 +1,9 @@
 package com.example.iotproject
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.PorterDuff
+import android.os.Build.VERSION_CODES.P
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,9 +11,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.iotproject.databinding.FragmentTabTvBinding
 
+
 class TabTvFragment : Fragment() {
     private var _binding: FragmentTabTvBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,8 @@ class TabTvFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var now_channel :Int = 0
+
 
         //전원
         binding.switchTv.setOnCheckedChangeListener{ p, isChecked ->
@@ -29,13 +35,18 @@ class TabTvFragment : Fragment() {
             }
         }
 
-        //TV 채널 초기 값
-        binding.txtChannelTv.setText("01")
+        //저장된 채널 번호 불러오기
+        loadData()
 
         //채널 위로 가는 버튼
         binding.btnChannelUpTv.setOnClickListener{
-            val now_channel = binding.txtChannelTv.text.toString().toInt() + 1
-            binding.txtChannelTv.setText(now_channel.toString())
+            now_channel = binding.txtChannelTv.text.toString().toInt() + 1
+            if(now_channel<10){
+                binding.txtChannelTv.setText("0"+now_channel.toString())
+            }
+            else{
+                binding.txtChannelTv.setText(now_channel.toString())
+            }
         }
 
         //채널 아래로 가는 버튼
@@ -54,8 +65,27 @@ class TabTvFragment : Fragment() {
         }
     }
 
+    private fun saveData(){
+        //채널 번호 저장하기
+        val pref = requireActivity().getPreferences(0)
+        val edit = pref.edit()
+        edit.putString("channel", binding.txtChannelTv.text.toString())
+        edit.apply()
+    }
+    private fun loadData(){
+        //채널 번호 불러오기
+        val pref = requireActivity().getPreferences(0)
+        binding.txtChannelTv.setText(pref.getString("channel", "01"))
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
         _binding = FragmentTabTvBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //Fragment가 종료되기 직전에 채널 저장
+        saveData()
     }
 }
